c44267485477c5de4c7d86e176f693f68f1c4df5
Simon Génier <simon.genier@jadedpixel.com>
2014-05-28 02:00:32 +0000

    Obviously, ^ -> \A

--- lib/liquider/tokens.rb
+++ lib/liquider/tokens.rb
@@ -12,79 +12,79 @@ module Liquider::Tokens
 
   class Ident < Token
     def self.pattern
-      %r<^(?:[[:alpha:]]|_)(?:[[[:alpha:]][[:digit:]]-_])*(?:!|\?)?>
+      %r<\A(?:[[:alpha:]]|_)(?:[[[:alpha:]][[:digit:]]-_])*(?:!|\?)?>
     end
   end
 
   class MustacheOpen < Token
     def self.pattern
-      %r<^{%>
+      %r<\A{%>
     end
   end
 
   class MustacheClose < Token
     def self.pattern
-      %r<^%}>
+      %r<\A%}>
     end
   end
 
   class DoubleMustacheOpen < Token
     def self.pattern
-      %r<^{{>
+      %r<\A{{>
     end
   end
 
   class DoubleMustacheClose < Token
     def self.pattern
-      %r<^}}>
+      %r<\A}}>
     end
   end
 
   class ParenOpen < Token
     def self.pattern
-      %r<^\(>
+      %r<\A\(>
     end
   end
 
   class ParenClose < Token
     def self.pattern
-      %r<^\)>
+      %r<\A\)>
     end
   end
 
   class Range < Token
     def self.pattern
-      %<^\.\.>
+      %<\A\.\.>
     end
   end
 
   class BinaryOp < Token
     def self.pattern
-      %r{^(?:(?:==)|(?:/=)|(?:>=)|(?:<=)|(?:>)|(?:<)|\+|-|\*|/)}
+      %r{\A(?:(?:==)|(?:/=)|(?:>=)|(?:<=)|(?:>)|(?:<)|\+|-|\*|/)}
     end
   end
 
   class Filter < Token
     def self.pattern
-      %r<\|>
+      %r<\A\|>
     end
   end
 
   class Comma < Token
     def self.pattern
-      %r<,>
+      %r<\A,>
     end
   end
 
   class Quote < Token
     def self.pattern
-      %r<'>
+      %r<\A'>
     end
   end
 
   class DoubleQuote < Token
     def self.pattern
-      %r<">
+      %r<\A">
     end
   end
 
